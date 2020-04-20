19ad79049cccc728f79cd7587a7a0c39dc9994b5
ADFPGA <3361567500@qq.com>
2020-04-20 15:36:41 +0800

    hello

--- Gslution/quickSort.py
+++ Gslution/quickSort.py
@@ -1,1 +1,15 @@
-""
+"""
+what: quick sort
+"""
+def quicksort(arr):
+    if len(arr)<2:
+        return arr
+    else:
+        base_arr=arr[0]
+        smaller =[i for i in arr[1:] if i<base_arr]
+        biger = [i for i in arr[1:] if i>base_arr]
+        return quicksort(smaller) +[base_arr]+quicksort(biger)
+
+print(quicksort([10,1,4,5,7,2]))
+
+
